47c79c10d2205fb327da0c7f3181c47126b72725
OmarRayes98 <omarrayes1998@gmail.com>
2024-10-11 14:42:37 +0300

    lesson 39 add Clerk with Protected

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+  return (
+    <div className="inset-0 flex items-center justify-center min-h-[calc(100vh-80px)]">
+      <SignIn afterSignOutUrl={"/"}/>
+    </div>
+
+  )
+}
