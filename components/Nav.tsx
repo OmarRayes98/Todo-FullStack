1aa9958959e4a82fecb95329028778149816bdde
OmarRayes98 <omarrayes1998@gmail.com>
2024-10-11 15:25:15 +0300

    leeson 40 add logout with nav

---
+++ components/Nav.tsx
@@ -0,0 +1,20 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "./ModeToggle";
+
+const Nav = () => {
+  return (
+    <nav className="container flex items-center justify-between">
+        <ModeToggle/>
+      <SignedIn>
+        {/* Mount the UserButton component */}
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        {/* Signed out users get sign in button */}
+        <SignInButton />
+      </SignedOut>
+    </nav>
+  );
+};
+
+export default Nav;
